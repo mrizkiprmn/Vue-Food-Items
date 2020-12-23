f8a3ceb03a208fcebff011f211304c3795569f1a
mrizkiprmn <rizkiypermana@gmail.com>
2020-12-23 17:45:35 +0700

    change history

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="history">
-     
-    <side-nav :modalAdd="true" />
-     <header-item :text="'History'" :searchIcon="false"/>
-    <div class="table">
-    <b-table striped hover :items="items" :fields="fields"></b-table>
-  </div>
-    
-      
-     
-        
+    <side-nav :modalAdd="false" />
+   <section class="main-section">
+      <header-item :text="'History'" :searchicon="false" />
+      <main class="container">
+          <div class="row mt-4">
+        <div class="col-12 col-md-4">
+          <img class="card-img-top"  src="../assets/history/Card-1.svg" alt=""/>
+        </div>
+        <div class="col-12 col-md-4">
+          <img class="card-img-top"  src="../assets/history/Card-2.svg" alt=""/>
+        </div>
+        <div class="col-12 col-md-4">
+          <img class="card-img-top"  src="../assets/history/Card-3.svg" alt=""/>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-12">
+          <img src="../assets/history/Grafik.svg" alt="" class="card-img-top" />
+        </div>
+      </div>
+      <h2 class="text-left mt-3 p-2">Recent orders</h2>
+        <b-table striped hover :items="items" :fields="fields"></b-table>
+      </main>
+    </section>
   </div>
 </template>
 
 <script>
 import HeaderItem from "../components/Header.vue";
-import axios from "axios";  
+import axios from "axios";
 import SideNav from "../components/Navbar.vue";
 export default {
   name: "History",
@@ -23,40 +37,34 @@ export default {
     HeaderItem,
     SideNav,
   },
-  props:{
-    searchIcon: {
-      type : Boolean,
-      required: true,
-    }
-  },
   data() {
     return {
       fields: [
         {
           key: "invoice",
-          label: "Invoices",
+          label: "INVOICES",
           sortable: true,
         },
         {
           key: "cashier",
-          label: "Cashier",
+          label: "CASHIER",
           sortable: true,
         },
         {
           key: "date",
-          label: "Dates",
+          label: "DATES",
           sortable: true,
         },
         {
           key: "orders",
-          label: "ORDERS",
+          label: "ORDER",
           sortable: true,
         },
         {
           key: "amount",
           label: "AMOUNT (Rp)",
           sortable: true,
-          variant: "success",
+          variant: "info",
         },
       ],
       items: [],
@@ -64,39 +72,23 @@ export default {
   },
   mounted() {
     axios
-      .get('http://localhost:8888/history')
+      .get(process.env.VUE_APP_HISTORY)
       .then((res) => {
         this.items = res.data.result;
       })
       .catch((err) => {
-        alert(err.message);
+        console.log(err);
       });
   },
 };
 </script>
 
 <style scoped>
-.nav {
-  height: 1000px;
-}
 .history {
   margin: 0;
   padding: 0;
+  height: 100vh;
   display: grid;
   grid-template-columns: 80px auto;
-}
-
-.table {
-  align-items: center;
-  width: 1000px;
-  margin: 10px;
-  padding: 10px 420px; 
-}
-
-.header-nav {
-  margin: 10px 0;
-  position: sticky;
-  top: 15px;
-  height: 2000px;
 }
 </style>
