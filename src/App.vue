0ce80e28169bffa34bf152a2ae6cce4bc58cd99a
mrizkiprmn <rizkiypermana@gmail.com>
2020-12-23 18:05:20 +0700

    edit

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,9 @@
 #app::before,
 #appafter {
   font-family: sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-}
-
-h4 {
-  font-size: 1.3rem;
-}
-p {
-  font-size: 1rem;
 }
 </style>
